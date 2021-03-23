a97e5bee8102af64dcf31c544b5f5e0cebd8e196
eliasmatosg <eliasmg@protonmail.ch>
2021-03-23 20:04:41 -0300

    Adicionado o método de decodificação na classe.
    Foi adicionado um método capaz de decodificar a frase pós codificação do
    método adicionado previamente. Para decodificação é usado um comparador
    de posições de letras em dois alfabetos distintos.

--- Codificadores/Codifica17204017.java
+++ Codificadores/Codifica17204017.java
@@ -1,28 +1,41 @@
 package Codificadores;
 
-public class CodificaAA implements Codifica {
-    private String alfabetoQWERTY = "QWERTYUIOPASDFGHJKLÇZXCVBNMqwertyuiopasdfghjklçzxcvbnm";
-    private String alfabeto =       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+public class Codifica17204017 implements Codifica {
+    private String alfabetoQWERTY = "1234567890:.ŷéQWERTYUIOPASDFGHJKLÇZXCVBNMqwertyuiopasdfghjklçzxcvbnm";
+    private String alfabeto =       "1234567890:.ãúABCDEFGHIJKLMNOPQRSÇTUVWXYZabcdefghijklmnopqrsçtuvwxyz";
 
     @Override
     public String codifica(String str) {
         String textoCodificado = "";
-        for (int i = 0; i <str.lenght(); i++){
-            for (int j = 0; j < alfabetoQWERTY.lenght(); j++){
+        for (int i = 0; i < str.length(); i++){
+            for (int j = 0; j < alfabetoQWERTY.length(); j++){
                 if (str.charAt(i) == ' '){
                     textoCodificado += " ";
                     break;
                 }
-
-                else if (str.charAt(i) == alfabetoQWERTY.charAt(j)){
+                else if (str.charAt(i) == alfabeto.charAt(j)){
                     textoCodificado += alfabetoQWERTY.charAt(j);
                 }
             }
         }
+        return textoCodificado;
     }
 
     @Override
     public String decodifica(String str) {
+        String textoDecodificado = "";
+        for (int i = 0; i < str.length(); i++){
+            for (int j = 0; j < alfabeto.length(); j++){
+                if (str.charAt(i) == ' '){
+                    textoDecodificado += " ";
+                    break;
+                }
+                else if (str.charAt(i) == alfabetoQWERTY.charAt(j)){
+                    textoDecodificado += alfabeto.charAt(j);
+                }
+            }
+        }
+        return textoDecodificado;
     }
 
     @Override
