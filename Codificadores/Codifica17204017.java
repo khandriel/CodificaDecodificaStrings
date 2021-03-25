ccd39f0c6f0b0f8afa5f6035bbfb2c7d01c8f3ac
eliasmatosg <eliasmg@protonmail.ch>
2021-03-25 19:37:22 -0300

    prevista situação de não encontrar letra

--- Codificadores/Codifica17204017.java
+++ Codificadores/Codifica17204017.java
@@ -1,13 +1,11 @@
 package Codificadores;
 
 public class Codifica17204017 implements Codifica {
-
     private String alfabetoQWERTY = "单1234567890:. ŷéQWERTYUIOPASDFGHJKLÇZXCVBNMqwertyuiopasdfghjklçzxcvbnm";
     private String alfabeto =       "单1234567890:. ãúABCDEFGHIJKLMNOPQRSÇTUVWXYZabcdefghijklmnopqrsçtuvwxyz";
 
     @Override
     public String codifica(String str) {
-        char[] caracteres = str.toCharArray();
         String textoCodificado = "";
         for (int i = 0; i < str.length(); i++){
             int posicaoCaractere = alfabeto.indexOf(str.charAt(i));
@@ -20,11 +18,10 @@ public class Codifica17204017 implements Codifica {
 
     @Override
     public String decodifica(String str) {
-        char[] caracteres = str.toCharArray();
         String textoDecodificado = "";
-        for (char c : caracteres){
-            c -= chave;
-            textoDecodificado += c;
+        for (int i = 0; i < str.length(); i++){
+            int posicaoCaractere = alfabetoQWERTY.indexOf(str.charAt(i));
+            textoDecodificado += alfabeto.charAt(posicaoCaractere);
         }
         return textoDecodificado;
     }
@@ -40,3 +37,4 @@ public class Codifica17204017 implements Codifica {
     }
 
 }
+
