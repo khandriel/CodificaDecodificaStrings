43b4ef3e9b3309ffa7331d25ef9f3ca2d6ade6c3
Osmar <Osmar.S@edu,pucrs.br>
2021-03-29 18:28:23 -0300

    Criação do método codifica

--- Codificadores/Codifica20103647.java
+++ Codificadores/Codifica20103647.java
@@ -4,26 +4,25 @@ public class Codifica20103647 implements Codifica {
 
     private static String alfabetoInvertido = "zyxwvutsrqponmlkjihgfedcba0987654321ZYXWVUTSRQPONMLKJIHGFEDCBA";
     private String data = "03292021";
-    private int count = 0;
-    private int controle = 0;
+    private String [] dataSeparada = data.split("");
+    private int count;
 
     @Override
     public String codifica (String str){
         
         String frase = "";
+        count = 0;
+
         for(char a : str.toCharArray()){
             for(int i = 0; i < alfabetoInvertido.length(); i++){
                 if(a == alfabetoInvertido.charAt(i)){
-                    String chave = String.valueOf(data.charAt(i % 8));
-                    int chaveInt = Integer.parseInt(chave);
-                    if(count > 8) count -= 8;
-                    frase += alfabetoInvertido.charAt(i+chaveInt);
+                    int temp = count % 7;
+                    int chave = Integer.parseInt(dataSeparada[temp]);
+                    frase += alfabetoInvertido.charAt(i+chave);
                     count++;
-                    controle = 1;
                 }
-                else controle = 0;
             }
-            if(controle == 0) frase += a;
+            if(!Character.isLetterOrDigit(a)) frase += a;
         }
 
 
