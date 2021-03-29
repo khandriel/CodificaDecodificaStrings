917b4218b3251de0ea4768e797a81507f608018a
Osmar <Osmar.S@edu,pucrs.br>
2021-03-29 18:29:58 -0300

    Término da classe Codifica20103647

--- Codificadores/Codifica20103647.java
+++ Codificadores/Codifica20103647.java
@@ -28,5 +28,34 @@ public class Codifica20103647 implements Codifica {
 
         return frase;
     }
+
+    @Override
+    public String decodifica(String str){
+        String frase = "";
+        count = 0;
+        for(char a : str.toCharArray()){
+            for(int i = 0; i < alfabetoInvertido.length(); i++){
+                if(alfabetoInvertido.charAt(i) == a){
+                    int temp = count % 7;
+                    int chave = Integer.parseInt(dataSeparada[temp]);
+                    frase += alfabetoInvertido.charAt(i - chave);
+                    count ++;
+                }
+            }
+           if(! Character.isLetterOrDigit(a)) frase += a;
+        }
+
+
+        return frase;
+    }
     
+    @Override
+    public String getMatriculaAutor(){
+        return "20103647";
+    }
+
+    @Override
+    public String getNomeAutor(){
+        return "Osmar Nether";
+    }
 }
